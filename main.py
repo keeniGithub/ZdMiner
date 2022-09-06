c472dca85a0190a0bf2c7a0db6ec32ae3d1a4d1c
keeniHub <100033213+keeniGithub@users.noreply.github.com>
2022-09-06 16:22:48 -0700

    Zd miner on the python
    This is not a real mainer, this is just my work in python

---
+++ main.py
@@ -0,0 +1,66 @@
+import time as t  # подключения Библиотеки для управленияем времени
+import keyboard as keyb # Подключения библиотеки для управлением с клавиатуры
+# Создаем переменные
+zd = 0 
+i =0
+# Пишем текст для устоновки 
+print("Добро пожаловать в устоновшик Zd-coins mainer! \n")
+print("Чтоб начать устоновку нажмите Ctrl")
+# Делаем так, что при нажатии на ctrl начинается установка 
+keyb.wait('ctrl') # Ждём пока будет нажата клавиша ctrl
+print('\nУстоновка началась...')
+t.sleep(2.7) # Задержка 2,7 сек
+# Начала устоновки фаилов
+print('\nzd.dll')
+t.sleep(0.8)
+print('\nmain.dll')
+print('\nsttz.sys')
+print('\npy.dpy')
+print('\nsett.dll')
+t.sleep(0.6)
+print('\npython.dll')
+print('\ndebug.sys')
+t.sleep(1.8)
+print('\nsettingZd.dll')
+print('\npyZd.dll')
+print('\nkeybozd.dll')
+t.sleep(0.6)
+print('\nsystems.dll')
+print('\nsystems.sys')
+print('\nУстоновка завершина!\n Чтоб начать майнинг нажмите alt') #конец устоновки 
+keyb.wait('alt') # ждём пока будет нажата клавиша alt 
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+print("\n           ")
+# начало майнинга 
+print("\nМайниг начался...")
+# алгоритм майнинга 
+while zd >= 0:
+    t.sleep(0.9)
+    zd=zd+1
+    print("zd coins = " + str(zd))
+# Остоновка на случий переполнения переменной
+    if zd == 32614:
+        t.sleep(2.5)
+        print("Error varible: -294")
+        t.sleep(6)
+        print("Ñ‡Ð¾Ðº Ð¿Ð¾Ð´ÑŠÑ‘Ð¼Ñƒ ÑÐµÐ»ÑŒÑ")
+        t.sleep(993242)

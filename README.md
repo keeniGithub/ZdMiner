79d73a1828d4376f0c7efd0df0f79225a694133f
keeniHub <100033213+keeniGithub@users.noreply.github.com>
2022-10-16 13:11:55 +0300

    ZdMiner
    ZdMiner update 1.2.1. Random update

---
+++ README.md
@@ -0,0 +1,36 @@
+
+<h1 align="center">
+  <b>Zd Miner</b>
+</h1>
+
+<p align="center">
+  <b>version: 1.2.1</b><br>
+
+## In version 1.2.1, the random house function was added
+
+___
+
+<h2 align="center">
+  <b>libraries used in code:</b>
+</h2>
+
+```python
+import keyboard #libery for keyboard controls
+import time # libery for delay
+import random # libery for random time and charges Zd coins
+```
+
+<h2 align="center">
+  <b>Algorithm for accruing coins:</b>
+</h2>
+
+```python
+while True:  
+    zd=zd+uniform(0,0.5)
+    print("Zd Coins = " + str(zd))
+    t.sleep(uniform(3,10)) 
+```
+_______
+<p align="center">
+  <a href="https://discord.gg/zn5usa2GAc">Discord Server</a>
+
--- main.py
+++ main.py
@@ -1,66 +1,17 @@
-import time as t  # подключения Библиотеки для управленияем времени
-import keyboard as keyb # Подключения библиотеки для управлением с клавиатуры
-# Создаем переменные
-zd = 0 
-i =0
-# Пишем текст для устоновки 
-print("Добро пожаловать в устоновшик Zd-coins mainer! \n")
-print("Чтоб начать устоновку нажмите Ctrl")
-# Делаем так, что при нажатии на ctrl начинается установка 
-keyb.wait('ctrl') # Ждём пока будет нажата клавиша ctrl
-print('\nУстоновка началась...')
-t.sleep(2.7) # Задержка 2,7 сек
-# Начала устоновки фаилов
-print('\nzd.dll')
-t.sleep(0.8)
-print('\nmain.dll')
-print('\nsttz.sys')
-print('\npy.dpy')
-print('\nsett.dll')
-t.sleep(0.6)
-print('\npython.dll')
-print('\ndebug.sys')
-t.sleep(1.8)
-print('\nsettingZd.dll')
-print('\npyZd.dll')
-print('\nkeybozd.dll')
-t.sleep(0.6)
-print('\nsystems.dll')
-print('\nsystems.sys')
-print('\nУстоновка завершина!\n Чтоб начать майнинг нажмите alt') #конец устоновки 
-keyb.wait('alt') # ждём пока будет нажата клавиша alt 
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-print("\n           ")
-# начало майнинга 
-print("\nМайниг начался...")
-# алгоритм майнинга 
-while zd >= 0:
-    t.sleep(0.9)
-    zd=zd+1
-    print("zd coins = " + str(zd))
-# Остоновка на случий переполнения переменной
-    if zd == 32614:
-        t.sleep(2.5)
-        print("Error varible: -294")
-        t.sleep(6)
-        print("Ñ‡Ð¾Ðº Ð¿Ð¾Ð´ÑŠÑ‘Ð¼Ñƒ ÑÐµÐ»ÑŒÑ")
-        t.sleep(993242)
+import keyboard as keyb
+import time as t 
+from random import *
+zd = 0
+print("Welcome to Zd coins miner!")
+t.sleep(1.5)
+print("Click to F7 for start mining")
+keyb.wait('F7')
+t.sleep(2.2)
+print("Preparations in progress... ")
+t.sleep(3.5)
+print("Mining has started")
+t.sleep(1.5)
+while True:  
+    zd=zd+uniform(0,0.5)
+    print("Zd Coins = " + str(zd))
+    t.sleep(uniform(3,10))    
